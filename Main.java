00c764e9b5f15a8bbee3a3122e8afae6e30bb8b2
Colin Johnson <colin@colinjohnson.net>
2019-01-31 14:18:15 -0800

    restructured files

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("starting game...");
+		
+		// create a frame to contain game
+		JFrame container = new JFrame("Desert escape");
+		
+		// make a new game.
+		@SuppressWarnings("unused")
+		EscapeGame game = new EscapeGame(container);
+	}
+}
---
+++ Obstacle.java
@@ -0,0 +1,40 @@
+
+
+// an obstacle is an entity that can damage the player
+public class Obstacle extends Entity {
+	private EscapeGame game;
+	private boolean pastCollision = false; // the car cannot collide with an obstacle twice.
+	String type;
+
+	public Obstacle(EscapeGame g, String r, int newX, int newY, String obstacleType) {
+		super(r, newX, newY);
+		
+		type = obstacleType;
+		game = g;
+	}
+
+	@Override
+	public void collidedWith(Entity other) {
+		pastCollision = true;
+	}
+
+	public void move(long delta) {	
+		setY(getY() - game.gameDY);
+
+		// if obstacle moves off bottom of screen, remove it from ObstacleEntity list
+		if (y > 800) {
+			game.removeEntity(this);
+		} // if
+
+	} // move
+
+	public void setY(double newY) {
+		
+		//TODO: move method to entity
+		y = (int)newY;
+	}
+	
+	public boolean getPastCollision(){
+		return pastCollision;
+	}
+}
