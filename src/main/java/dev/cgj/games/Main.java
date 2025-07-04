af9330c100c3fc86f218d187ae3bcfb6a98c385a
Colin Johnson <colin@cgj.dev>
2025-07-04 00:12:45 -0700

    Move game logic into core and sprites into assets

--- src/main/java/dev/cgj/games/Main.java
+++
@@ -1,9 +0,0 @@
-package dev.cgj.games;
-
-import javax.swing.JFrame;
-
-public class Main {
-	public static void main(String[] args) {
-        new EscapeGame(new JFrame("Desert escape")).run();
-	}
-}
