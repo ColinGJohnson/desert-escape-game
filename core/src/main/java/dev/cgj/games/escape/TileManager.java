9947821d2a53321cf1fc3ecc6e0bdaa13d6d0420
Colin Johnson <colin@cgj.dev>
2025-08-11 01:05:16 -0700

    Add wall tiles to TileManager and update assets

--- core/src/main/java/dev/cgj/games/escape/TileManager.java
+++ core/src/main/java/dev/cgj/games/escape/TileManager.java
@@ -6,27 +6,49 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Disposable;
 
 public class TileManager implements Disposable {
-  private final Texture dirtTexture;
+  private final Texture roadTexture;
+  private final Texture wallTexture;
   private final float tileSize;
 
   public TileManager() {
-    dirtTexture = new Texture("sprites/terrain/ground.png");
-    tileSize = dirtTexture.getWidth() * Constants.PIXEL_SCALE;
+    roadTexture = new Texture("sprites/terrain/ground.png");
+    wallTexture = new Texture("sprites/terrain/wall.png");
+    tileSize = roadTexture.getWidth() * Constants.PIXEL_SCALE;
   }
 
   @Override
   public void dispose() {
-    dirtTexture.dispose();
+    roadTexture.dispose();
   }
 
   public void draw(SpriteBatch batch, Vector2 position) {
-    drawBackgroundTile(batch, new Vector2(position.x, position.y + tileSize));
-    drawBackgroundTile(batch, new Vector2(position.x, position.y));
-    drawBackgroundTile(batch, new Vector2(position.x, position.y - tileSize));
+    for (int i = -1; i <= 1; i++) {
+      float y = position.y + i * tileSize;
+      drawLeftWall(batch, new Vector2(-tileSize, y));
+      drawRoadTile(batch, new Vector2(0, y));
+      drawRightWall(batch, new Vector2(tileSize, y));
+    }
   }
 
-  private void drawBackgroundTile(SpriteBatch batch, Vector2 position) {
+  private void drawRoadTile(SpriteBatch batch, Vector2 position) {
+    Vector2 bottomLeft = roundToTileSize(position);
+    batch.draw(roadTexture, bottomLeft.x, bottomLeft.y, tileSize, tileSize);
+  }
+
+  public void drawLeftWall(SpriteBatch batch, Vector2 position) {
+    Vector2 bottomLeft = roundToTileSize(position);
+    batch.draw(wallTexture, bottomLeft.x, bottomLeft.y, tileSize, tileSize);
+  }
+
+  public void drawRightWall(SpriteBatch batch, Vector2 position) {
+    Vector2 bottomLeft = roundToTileSize(position);
+    batch.draw(wallTexture, bottomLeft.x, bottomLeft.y, tileSize, tileSize, 0, 0,
+      wallTexture.getWidth(), wallTexture.getHeight(), true, false);
+  }
+
+  private Vector2 roundToTileSize(Vector2 position) {
+    float x = Math.round(position.x / tileSize) * tileSize;
     float y = Math.round(position.y / tileSize) * tileSize;
-    batch.draw(dirtTexture, 0, y, tileSize, tileSize);
+    return new Vector2(x, y);
   }
 }
